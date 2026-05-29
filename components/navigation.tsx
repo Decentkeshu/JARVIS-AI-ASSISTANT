70250a09f945bf85dd4f34dd1d55285d06b80efe
keshav kumar <msd76017@gmail.com>
2026-05-29 19:29:10 +0530

    uninstall clerk

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { SignInButton, SignUpButton, UserButton, Show } from "@clerk/nextjs";
+// import { SignInButton, SignUpButton, UserButton, Show } from "@clerk/nextjs";
 import Link from "next/link";
 import { useState } from "react";
 
